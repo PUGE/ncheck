1909ebdc5449b27b10b205631772c30c96d4b125
PUGE <mmit7750>
2017-02-05 21:51:07 +0800

    。。。。

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,9 @@
-require('../lib/cli');
+#!/usr/bin/env node
+
+var isEs2015;
+try {
+    isEs2015 = new Function('() => {}');
+} catch (e) {
+    isEs2015 = false;
+}
+isEs2015 ? require('../lib/cli') : console.log("nodejs版本太低!");
