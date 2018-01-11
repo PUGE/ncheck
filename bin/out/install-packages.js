07b6e07186942046665295913b6a6675881b41e6
PUGE <100284685@qq.com>
2018-01-11 20:21:40 +0800

    完善

--- bin/out/install-packages.js
+++ bin/out/install-packages.js
@@ -2,10 +2,9 @@
 
 const chalk = require('chalk');
 const execa = require('execa');
-const inquirer = require('inquirer')
 const ora = require('ora');
 
-function install(packages, currentState) {
+function install(packages, currentState, source) {
   if (!packages.length) {
     return Promise.resolve(currentState);
   }
@@ -14,47 +13,36 @@ function install(packages, currentState) {
   const saveExact = currentState.get('saveExact') ? '--save-exact' : null;
   let npmArgs = null
 
-  const questions = [
-    {
-      name: 'source',
-      message: '选择更新方式.',
-      type: 'list',
-      choices: ['npm', 'yarn']
-    }
-  ]
+  // 待优化
+  if (source === 'npm') {
+    npmArgs = ['install']
+      .concat(installGlobal)
+      .concat(saveExact)
+      .concat(packages)
+      .filter(Boolean);
+  } else {
+    npmArgs = ['add']
+      .concat(installGlobal)
+      .concat(packages[1])
+      .filter(Boolean);
+  }
 
-  inquirer.prompt(questions).then(answers => {
-    // 待优化
-    if (answers.source === 'npm') {
-      npmArgs = ['install']
-        .concat(installGlobal)
-        .concat(saveExact)
-        .concat(packages)
-        .filter(Boolean);
-    } else {
-      npmArgs = ['add']
-        .concat(installGlobal)
-        .concat(packages[1])
-        .filter(Boolean);
-    }
+  console.log('')
+  console.log(`$ ${chalk.green(source)} ${chalk.green(npmArgs.join(' '))}`);
+  const spinner = ora(`正在通过 ${chalk.green(source)} 安装更新包!`);
+  spinner.enabled = spinner.enabled && currentState.get('spinner');
+  spinner.start();
 
-    console.log('')
-    console.log(`$ ${chalk.green(answers.source)} ${chalk.green(npmArgs.join(' '))}`);
-    const spinner = ora(`正在通过 ${chalk.green(answers.source)} 安装更新包!`);
-    spinner.enabled = spinner.enabled && currentState.get('spinner');
-    spinner.start();
+  return execa(source, npmArgs, {cwd: currentState.get('cwd')}).then(output => {
+      spinner.stop();
+      console.log(output.stdout);
+      console.log(output.stderr);
 
-    return execa(answers.source, npmArgs, {cwd: currentState.get('cwd')}).then(output => {
-        spinner.stop();
-        console.log(output.stdout);
-        console.log(output.stderr);
-
-        return currentState;
-    }).catch(err => {
-        spinner.stop();
-        throw err;
-    });
-  })
+      return currentState;
+  }).catch(err => {
+      spinner.stop();
+      throw err;
+  });
 }
 
 module.exports = install;
--- bin/out/interactive-update.js
+++ bin/out/interactive-update.js
@@ -36,128 +36,139 @@ const UI_GROUPS = [
 ];
 
 function label(pkg) {
-    const bumpInstalled = pkg.bump ? pkg.installed : '';
-    const installed = pkg.mismatch ? pkg.packageJson : bumpInstalled;
-    const name = chalk.yellow(pkg.moduleName);
-    const type = pkg.devDependency ? chalk.green(' devDep') : '';
-    const missing = pkg.notInstalled ? chalk.red(' missing') : '';
-    return [
-        name + type + missing,
-        installed,
-        installed && '❯',
-        chalk.bold(pkg.latest || '')
-    ];
+  const bumpInstalled = pkg.bump ? pkg.installed : '';
+  const installed = pkg.mismatch ? pkg.packageJson : bumpInstalled;
+  const name = chalk.yellow(pkg.moduleName);
+  const type = pkg.devDependency ? chalk.green(' devDep') : '';
+  const missing = pkg.notInstalled ? chalk.red(' missing') : '';
+  return [
+    name + type + missing,
+    installed,
+    installed && '❯',
+    chalk.bold(pkg.latest || '')
+  ];
 }
 
 function short(pkg) {
-    return `${pkg.moduleName}@${pkg.latest}`;
+  return `${pkg.moduleName}@${pkg.latest}`;
 }
 
 function choice(pkg) {
-    if (!pkg.mismatch && !pkg.bump && !pkg.notInstalled) {
-        return false;
-    }
+  if (!pkg.mismatch && !pkg.bump && !pkg.notInstalled) {
+    return false;
+  }
 
-    return {
-        value: pkg,
-        name: label(pkg),
-        short: short(pkg)
-    };
+  return {
+    value: pkg,
+    name: label(pkg),
+    short: short(pkg)
+  };
 }
 
 function unselectable(options) {
-    return new inquirer.Separator(chalk.reset(options ? options.title : ' '));
+  return new inquirer.Separator(chalk.reset(options ? options.title : ' '));
 }
 
 function createChoices(packages, options) {
-    const filteredChoices = _.filter(packages, options.filter);
+  const filteredChoices = _.filter(packages, options.filter);
 
-    const choices = filteredChoices.map(choice)
-        .filter(Boolean);
+  const choices = filteredChoices.map(choice)
+    .filter(Boolean);
 
-    const choicesAsATable = table(_.map(choices, 'name'), {
-        align: ['l', 'l', 'l'],
-        stringLength: function (str) {
-            return stripAnsi(str).length;
-        }
-    }).split('\n');
-
-    const choicesWithTableFormating = _.map(choices, (choice, i) => {
-        choice.name = choicesAsATable[i];
-        return choice;
-    });
-
-    if (choicesWithTableFormating.length) {
-        choices.unshift(unselectable(options));
-        choices.unshift(unselectable());
-        return choices;
+  const choicesAsATable = table(_.map(choices, 'name'), {
+    align: ['l', 'l', 'l'],
+    stringLength: function (str) {
+      return stripAnsi(str).length;
     }
+  }).split('\n');
+
+  const choicesWithTableFormating = _.map(choices, (choice, i) => {
+    choice.name = choicesAsATable[i];
+      return choice;
+  });
+
+  if (choicesWithTableFormating.length) {
+    choices.unshift(unselectable(options));
+    choices.unshift(unselectable());
+    return choices;
+  }
 }
 
 function interactive(currentState) {
-    const packages = currentState.get('packages');
+  const packages = currentState.get('packages');
 
-    const choicesGrouped = UI_GROUPS.map(group => createChoices(packages, group))
-        .filter(Boolean);
+  const choicesGrouped = UI_GROUPS.map(group => createChoices(packages, group))
+    .filter(Boolean);
 
-    const choices = _.flatten(choicesGrouped);
+  const choices = _.flatten(choicesGrouped);
 
-    if (!choices.length) {
-        console.log(`owo这个项目所引用的模块都是最新的！`);
-        return;
+  if (!choices.length) {
+    console.log(`owo这个项目所引用的模块都是最新的！`);
+    return;
+  }
+
+  choices.push(unselectable());
+  choices.push(unselectable({title: '空格(Space)勾选. 回车(Enter)开始更新. Control-C放弃操作.'}));
+
+  const questions = [
+    {
+      name: 'packages',
+      message: '勾选需要更新的模块.',
+      type: 'checkbox',
+      choices: choices.concat(unselectable()),
+      pageSize: process.stdout.rows - 2
+    }
+  ];
+  return inquirer.prompt(questions).then(answers => {
+    const packagesToUpdate = answers.packages;
+    
+    if (!packagesToUpdate || !packagesToUpdate.length) {
+      console.log('没有选中需要更新的项目.');
+      return false;
     }
 
-    choices.push(unselectable());
-    choices.push(unselectable({title: '空格(Space)勾选. 回车(Enter)开始更新. Control-C放弃操作.'}));
+    const saveDependencies = packagesToUpdate
+      .filter(pkg => !pkg.devDependency)
+      .map(pkg => pkg.moduleName + '@' + pkg.latest);
 
+    const saveDevDependencies = packagesToUpdate
+      .filter(pkg => pkg.devDependency)
+      .map(pkg => pkg.moduleName + '@' + pkg.latest);
+
+    const updatedPackages = packagesToUpdate
+      .map(pkg => pkg.moduleName + '@' + pkg.latest).join(', ');
+
+    if (!currentState.get('global')) {
+      if (saveDependencies.length) {
+        saveDependencies.unshift('--save');
+      }
+
+      if (saveDevDependencies.length) {
+        saveDevDependencies.unshift('--save-dev');
+      }
+    }
     const questions = [
       {
-        name: 'packages',
-        message: '勾选需要更新的模块.',
-        type: 'checkbox',
-        choices: choices.concat(unselectable()),
-        pageSize: process.stdout.rows - 2
+        name: 'source',
+        message: '选择更新方式.',
+        type: 'list',
+        choices: ['npm', 'yarn']
       }
-    ];
-    return inquirer.prompt(questions).then(answers => {
-        const packagesToUpdate = answers.packages;
-        
-        if (!packagesToUpdate || !packagesToUpdate.length) {
-            console.log('没有选中需要更新的项目.');
-            return false;
-        }
-
-        const saveDependencies = packagesToUpdate
-            .filter(pkg => !pkg.devDependency)
-            .map(pkg => pkg.moduleName + '@' + pkg.latest);
-
-        const saveDevDependencies = packagesToUpdate
-            .filter(pkg => pkg.devDependency)
-            .map(pkg => pkg.moduleName + '@' + pkg.latest);
-
-        const updatedPackages = packagesToUpdate
-            .map(pkg => pkg.moduleName + '@' + pkg.latest).join(', ');
-
-        if (!currentState.get('global')) {
-            if (saveDependencies.length) {
-                saveDependencies.unshift('--save');
-            }
-
-            if (saveDevDependencies.length) {
-                saveDevDependencies.unshift('--save-dev');
-            }
-        }
-
-        return installPackages(saveDependencies, currentState)
-            .then(currentState => installPackages(saveDevDependencies, currentState))
-            .then(currentState => {
-                console.log('');
-                console.log(chalk.green(`[ncheck] 更新完成!`));
-                console.log(chalk.green('[ncheck] ' + updatedPackages));
-                console.log(chalk.green(`[ncheck] 建议你再次检查模块版本确保模块已经正常安装完毕.`));
-                return currentState;
-            });
-    });
+    ]
+    inquirer.prompt(questions).then(answers => {
+      const source = answers.source
+      return installPackages(saveDependencies, currentState, source)
+        .then(currentState => installPackages(saveDevDependencies, currentState, source))
+        .then(currentState => {
+          console.log('');
+          console.log(chalk.green(`[ncheck] 更新完成!`));
+          console.log(chalk.green('[ncheck] ' + updatedPackages));
+          console.log(chalk.green(`[ncheck] 建议你再次检查模块版本确保模块已经正常安装完毕.`));
+          return currentState;
+        });
+    })
+    
+  });
 }
 
 module.exports = interactive;
